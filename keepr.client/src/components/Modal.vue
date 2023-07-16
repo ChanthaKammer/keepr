af459184d6d8e11be2c98d06d6e24625d7e38460
Chantha Kammer <cadetsa219@gmail.com>
2023-07-15 19:54:54 -0600

    modal updates

--- keepr.client/src/components/Modal.vue
+++ keepr.client/src/components/Modal.vue
@@ -1,9 +1,9 @@
 <template>
-   <div class="modal fade" id="createKeepModal" tabindex="-1" aria-labelledby="createKeepModal" aria-hidden="true">
+   <div class="modal fade" id="modalTemplate" tabindex="-1" aria-labelledby="modalTemplate" aria-hidden="true">
       <div class="modal-dialog">
          <div class="modal-content">
             <div class="modal-header">
-            <h1 class="modal-title fs-5" id="createKeep">Placeholder</h1>
+            <!-- <h1 class="modal-title fs-5" id="createKeep">Placeholder</h1> -->
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <!-- <div class="modal-body">
