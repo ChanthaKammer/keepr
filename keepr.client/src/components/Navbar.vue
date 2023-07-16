8331e518d0b4712eadda72da7571a84bc55b96e3
Chantha Kammer <cadetsa219@gmail.com>
2023-07-16 10:23:15 -0600

    Navbar work

--- keepr.client/src/components/Navbar.vue
+++ keepr.client/src/components/Navbar.vue
@@ -3,20 +3,24 @@
     <router-link class="navbar-brand d-flex" :to="{ name: 'Home' }">
       <div class="d-flex flex-column align-items-center">
         <!-- <img alt="logo" src="../assets/img/cw-logo.png" height="45" /> -->
-        <h1 class="f-inter fs-1">Keepr</h1>
+        <h1 class="f-inter fs-1">Home</h1>
       </div>
     </router-link>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
       aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
+    <div class="dropdown">
+      <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+        Dropdown button
+      </button>
+      <ul class="dropdown-menu">
+        <li><a class="dropdown-item" href="#">New Keep</a></li>
+        <li><a class="dropdown-item" href="#">New Vault</a></li>
+      </ul>
+    </div>
     <div class="collapse navbar-collapse" id="navbarText">
       <ul class="navbar-nav me-auto">
-        <li>
-          <router-link :to="{ name: 'About' }" class="btn text-success lighten-30 selectable text-uppercase">
-            About
-          </router-link>
-        </li>
       </ul>
       <!-- LOGIN COMPONENT HERE -->
       <Login />
